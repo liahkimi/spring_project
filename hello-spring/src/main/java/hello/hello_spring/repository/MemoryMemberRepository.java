bfa79ba6533fdda30cd50a75ce56de474ccd90b5
kimgaeun <maruanna1994@gmail.com>
2025-01-03 17:50:13 +0900

    second commit

--- hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -2,10 +2,7 @@ package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
 
@@ -14,25 +11,28 @@ public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 8L;
 
-    @Override
+    @Override // 회원 저장 
     public Member save(Member member) {
         member.setId(sequence++); //회원번호 증가
         store.put(member.getId(),member); //회원의 이름을 가져와서 저장
         return member;
     }
 
-    @Override
+    @Override // 회원번호로 회원 찾기
     public Optional<Member> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(store.get(id));
     }
 
-    @Override
+    @Override // 회원이름으로 회원찾기
     public Optional<Member> findByName(String name) {
-        return Optional.empty();
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); // 이름으로 찾으면 반환
     }
 
-    @Override
+    @Override // 모든 회원 조회
     public List<Member> findAll() {
-        return List.of();
+        return new ArrayList<>(store.values());
+
     }
 }
---
+++ hello-spring/src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,21 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+}
